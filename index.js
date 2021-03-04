9e09c477f3a5b3d2dad9ac8f0df86b6064b73f9a
TREV-MSI-LAPTOP\GE62VR <trevorchico@gmail.com>
2021-03-03 19:19:50 -0800

    pg connect again

--- index.js
+++ index.js
@@ -10,6 +10,29 @@ var Authrouter = require('./Authrouter.js');
 
 const { Client } = require('pg');
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect(err => {
+  if (err) {
+    console.error('connection errorasdfasdf', err.stack)
+  } else {
+    console.log('connectedasdf')
+  }
+})
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
+
 //const client = new Client({
 //  connectionString: process.env.DATABASE_URL,
 //  ssl: {
